c3ae745411123dab293b281a6b8265c25e67213e
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-13 16:56:09 +0530

    worked on content descrption and practiced with flex-box

--- src/App.js
+++ src/App.js
@@ -134,6 +134,64 @@ function App() {
               <div className="overview__rating-count">429 votes</div>
             </div>
           </div>
+          <div className="detail">
+            <div className="description">
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+                nisi dignissimos debitis ratione sapiente saepe. Accusantium
+                cumque, quas, ut corporis incidunt deserunt quae architecto
+                voluptate.
+              </p>
+              <p className="paragraph">
+                Accusantium cumque, quas, ut corporis incidunt deserunt quae
+                architecto voluptate delectus, inventore iure aliquid aliquam.
+              </p>
+              <ul className="list">
+                <li className="list__item">Close to the beach</li>
+                <li className="list__item">Breakfast included</li>
+
+                <li className="list__item">Free airport shuttle</li>
+
+                <li className="list__item">Free wifi in all the room</li>
+
+                <li className="list__item">Air conditioning and heating</li>
+
+                <li className="list__item">Pets allowed</li>
+
+                <li className="list__item">we speak all languages</li>
+
+                <li className="list__item">Perfect for families</li>
+              </ul>
+              <div className="recommend">
+                <p className="recommend__count">
+                  Surya and 3 other friends recommend this hotel.
+                </p>
+                <div className="recommend__friends">
+                  <img
+                    src="user-3.jpg"
+                    alt="Friend 1"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src="user-4.jpg"
+                    alt="Friend 2"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src="user-5.jpg"
+                    alt="Friend 3"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src="user-6.jpg"
+                    alt="Friend 4"
+                    className="recommend__photo"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="user-reviews">User Reviews</div>
+          </div>
         </main>
       </div>
     </div>
